1559fdcf3e40eacc8461b69588517fea1734ca9d
Brin29 <breinerstivenparracortes50@gmail.com>
2024-12-08 22:06:23 -0500

    configuracion del servicio de student

--- microservice-student/src/main/java/com/microservice/student/MicroserviceStudentApplication.java
+++ microservice-student/src/main/java/com/microservice/student/MicroserviceStudentApplication.java
@@ -2,7 +2,10 @@ package com.microservice.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+// Cliente de eureka
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MicroserviceStudentApplication {
 
