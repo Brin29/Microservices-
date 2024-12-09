521f69455557a7750e4902191f991fd5c099c666
Brin29 <breinerstiveparracortes50@gmail.com>
2024-12-09 15:36:09 -0500

    as

--- microservice-course/src/main/java/com/microservice/course/client/StudentClient.java
+++ microservice-course/src/main/java/com/microservice/course/client/StudentClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 
 // Microservicio al cual se le hace la consulta
-@FeignClient(name = "msvc-student", url = "localhost:8091/api/student")
+@FeignClient(name = "msvc-student", url = "localhost:8080/api/student")
 public interface StudentClient {
 
     @GetMapping("/search-by-course/{idCourse}")
